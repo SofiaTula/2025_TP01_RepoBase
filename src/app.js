08ac9b9b74a5778e79b45ccb28b966bbab89dcfd
Sofia Tula <2320454@ucc.edu.ar>
2025-08-15 17:23:50 -0300

    Agregue la funcion de resta

--- src/app.js
+++ src/app.js
@@ -8,11 +8,15 @@ function sumar(a, b) {
     return a + b;
 }
 
+function resta(a,b){
+    return a - b;
+}
+
 // Ejemplo de uso
-let numero1 = 4;
+let numero1 = 8;
 let numero2 = 2;
 
 console.log("La suma es: " + sumar(numero1, numero2));
-
+console.log("La resta es: " - resta(numero1, numero2));
 
 saludar();
