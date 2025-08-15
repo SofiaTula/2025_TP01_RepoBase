d251bc2e84aca481fb468b1099f3c140f48e12ca
Sofia Tula <2320454@ucc.edu.ar>
2025-08-15 17:07:28 -0300

    agregue funcion para sumar

--- src/app.js
+++ src/app.js
@@ -3,4 +3,16 @@ function saludar() {
     console.log("Hola mundo");
 }
 
+// Función para sumar dos números
+function sumar(a, b) {
+    return a + b;
+}
+
+// Ejemplo de uso
+let numero1 = 4;
+let numero2 = 2;
+
+console.log("La suma es: " + sumar(numero1, numero2));
+
+
 saludar();
